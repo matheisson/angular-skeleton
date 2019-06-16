be8b6c76be3772bc1aa34b1c4a22d0ee439bd731
matheisson <csanyil@gmail.com>
2019-06-16 22:14:09 +0200

    directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,11 +10,12 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
   serverName = "test";
+  serverCreated = false;
 
   constructor() {
       setTimeout(() => {
           this.allowNewServer = true;
-      }, 2000);
+      }, 1000);
   }
 
   ngOnInit() {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
       this.serverCreationStatus = "Server was created";
+      this.serverCreated = true;
   }
 
   onUpdateServerName(event: any) {
